484ba914fdfce22be702700e8e0fbe3028c7dd37
sharjeel Zubair <sharjeel.zubair@virtualforce.io>
2019-09-18 18:20:13 +0500

    for aws made changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv/config');
 const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
 
 //body parser
 app.use(express.json());
@@ -18,7 +17,6 @@ app.use('/user',UserRoutes);
 
 
 //connect to DB
-
 mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology:true  }, ()=>{
    
     console.log('DB Connected (MLAB)');
